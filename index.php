65d348c789a6cd57dad97af31f18ab5ceedd2d55
anon <57300214+anon1277@users.noreply.github.com>
2022-07-17 11:31:32 -0700

    Create index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   <body>
   
     <?php
-    echo"hello  , i am bug hunter i have succefully take over your  subdomain dc.aiven.io"
+    echo "hello  , i am bug hunter i have succefully take over your  subdomain dc.aiven.io";
     ?>
     
   </body>
